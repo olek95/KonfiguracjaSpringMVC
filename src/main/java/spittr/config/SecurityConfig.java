8384f0c72a4770144c28afa107586df1d660602d
olek95 <olek1995s@poczta.onet.pl>
2018-07-28 19:56:47 +0200

    Praca z baza uzytkownikow zapisana w pamieci

--- src/main/java/spittr/config/SecurityConfig.java
+++ src/main/java/spittr/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package spittr.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
@@ -20,5 +21,19 @@ Posiada on nastêpuj¹ce metody do nadpisania: configure(WebSecurity) - konfigurac
 configure(AuthenticationManagerBuilder) - konfiguracja us³ug szczegó³ów u¿ytkownika. 
 Domyœlna implementacja 2 wersji blokuje przed jakimkolwiek wejœciem do aplikacji. */
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
+    @Override
+    /* KLasa AuthenticationManagerBuilder udostêpnia kilka metod umo¿liwiaj¹cych 
+    konfiguracjê mechanizmów Spring Security. */
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /* InmemoryAuthentication pozwala w³¹czyæ i skonfigurowaæ bazê u¿ytkowników 
+        zapisan¹ w pamiêci. WithUser tworzy u¿ytkownika. Jako parametr przyjmuje 
+        jego nazwê, a zwraca obiekt typu UserDetailsManagerConfigurer.UserDetailsBuilder, 
+        który udostêpnia metody umo¿liwiaj¹ce dalsz¹ konfiguracjê u¿ytkownika. 
+        Metoda password okreœla has³o, a roles przydziela uprawnienia. Istnieje 
+        podobna metoda - authorities w której role przydziela siê poprzedzaj¹c je 
+        prefiksem ROLE_ np. ROLE_USER. Konfiguracje ustawieñ poszczególnych 
+        u¿ytkowników rozdzielone s¹ za pomoc¹ metody and. */
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
+                .and().withUser("admin").password("password").roles("USER", "ADMIN");
+    }
 }
