6a06fffa7c5822c759bf2dc7a60317db2b0f7652
olek95 <olek1995s@poczta.onet.pl>
2018-08-23 19:29:46 +0200

    Deklarowanie fabryki sesji Hibernate

--- src/main/java/spittr/config/DataSourceConfiguration.java
+++ src/main/java/spittr/config/DataSourceConfiguration.java
@@ -1,12 +1,14 @@
 package spittr.config;
 
 import java.sql.SQLException;
+import java.util.Properties;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 
 @Configuration
 public class DataSourceConfiguration {
@@ -46,5 +48,29 @@ public class DataSourceConfiguration {
     @Bean
     public NamedParameterJdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
+    }
+    
+    /*LocalSessionFactoryBean jest jedn¹ z fabryk sesji dostêpnych w Hibernate. 
+    Implementacja SessionFactory jest odpowiedzialna za otwieranie i zamykanie sesji
+    Hibernate, które umo¿liwiaj¹ wykonywanie czynnoœci na bazie danych. Wybrany typ
+    umo¿liwia mapowanie obiektów na wpisy bazy zarówno w XML, jak i jako adnotacje. 
+    Jeœli wybierze siê t¹ sam¹ klasê ale z pakietu org.springframework.orm.hibernate3
+    to bêdzie umo¿liwia³a tylko mapowanie w XML. W wersji Hibernate od 3.2 do 4 trzeba 
+    wtedy u¿yæ AnnotationSessionFactoryBean które umo¿liwia zapis w postaci adnotacji. */
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean sfb = new LocalSessionFactoryBean();
+        // Ustawia referencê do Ÿród³a danych 
+        sfb.setDataSource(dataSource);
+        /* Ustawia pakiety w których poszukiwane s¹ klasy oznaczone jako utrwalane 
+        przez Hibernate, w³¹czaj¹c w to klasy z adnotacjami JPA @Entity i @MappedSuperClass 
+        oraz w³asn¹ adnotacj¹ Hibernate @Entity */ 
+        sfb.setPackagesToScan(new String[]{"spittr.domain"});
+        Properties props = new Properties();
+        props.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+        /* ustawia szczegó³owe wytyczne dla Hibernate, tu informujemy ¿e bêdzie 
+        wspó³pracowa³ z baz¹ MySQL */ 
+        sfb.setHibernateProperties(props);
+        return sfb;
     }
 }
